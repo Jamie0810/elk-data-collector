877a8f32a399d47e0f0eda059c32b9a16c050c48
Jamie <lijiachi03@gmail.com>
2021-06-26 20:41:27 +0800

    Add lambda handler test

--- src/go.mod
+++ src/go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/aws/aws-lambda-go v1.12.0
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/djhworld/go-lambda-invoke v0.0.3
+	github.com/stretchr/testify v1.3.0
 )
--- src/main_test.go
+++ src/main_test.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"context"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestExample(t *testing.T) {
+func TestLambdaHandler(t *testing.T) {
+	ctx := context.Background()
 
+	event := Event{
+		TraceId: "123",
+	}
+
+	resp, err := LambdaHandler(ctx, event)
+	if err != nil {
+		t.Fatalf("Job failed: %v", err)
+	}
+	assert.Equal(t, event.TraceId, resp.TraceId)
 }
